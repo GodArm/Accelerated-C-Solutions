b25ca87a598280a0e7ee0440590a636f668c6daf
Krishna Teja Salimeda <krishnasalimeda@gmail.com>
2015-04-03 21:06:33 +0530

    Added 0_5

--- 0_5.cpp
+++ 0_5.cpp
@@ -1,14 +1,10 @@
 //0-5
 #include <iostream>
+int main()   std::cout << "Hello World!" << std::endl;
 
-int main()
-{
-	std::cout<<"//A small C++ program"<<std::endl;
-	std::cout<<"#include <iostream>"<<std::endl;
-	std::cout<<"int main()"<<std::endl;
-	std::cout<<"{"<<std::endl;
-	std::cout<<"\tstd::cout<<\"Hello, World!\"<<std::endl;"<<std::endl;
-	std::cout<<"\treturn 0;"<<std::endl;
-	std::cout<<"}"<<std::endl;
-	return 0;
-}
+/*
+No its not a valid C++ program.
+You get an error:
+[Error] expected initializer before 'std'
+To correct the program, simply add the curly braces and enclose the statements within these curly braces.
+*/
